a4255296013e48bd26055c6a271b10357455bcf4
aliibnemasud <mdmasudrony@gmail.com>
2023-02-12 12:49:33 +0600

    code updated

--- routes/v1/product.route.js
+++ routes/v1/product.route.js
@@ -3,29 +3,47 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const Product = require("../../models/Product");
 
-
-router.route("/product")
+router
+  .route("/product")
+  /**
+   * @api {get} Get all the Products
+   */
   .get(async (req, res, next) => {
-
     try {
-      const result = await Product.find({})
-      res.send(result);      
-
+      const result = await Product.find({});
+      res.status(200).json({
+        status: "Success!",
+        message: "Product data Loaded Successfully!",
+        data: result,
+      });
     } catch (error) {
-        console.log(error)
+      res.status(400).send({
+        status: "Field!",
+        message: "Data inserted Fail",
+        error: error.message,
+      });
     }
-
   })
 
+  /**
+   * @api {Post} the Products
+   */
+
   .post(async (req, res, next) => {
-
     try {
-
-        const product = await Product.create(req.body)
-        res.send({status: 'success'})
-        
+      const product = await Product.create(req.body);
+      res.status(200).json({
+        status: "Success!",
+        message: "Product data Inserted Successfully!",
+        data: product,
+      });
     } catch (error) {
-        console.log(error)        
+
+      res.status(400).send({
+        status: "Field!",
+        message: "Data inserted Fail",
+        error: error.message,
+      });
     }
   });
 
